35bc1785085843ea97c7176d368f29f2db2d92be
mbarrajonmartinez@gmail.com <mbarrajonmartinez@gmail.com>
2017-02-22 09:44:07 +0100

    Modified splas time

--- app/src/main/java/com/example/dancd/aulamagnaapp/SplashActivity.java
+++ app/src/main/java/com/example/dancd/aulamagnaapp/SplashActivity.java
@@ -10,7 +10,7 @@ import java.util.TimerTask;
 public class SplashActivity extends Activity {
 
 
-    private static final long SPLASH_SCREEN_DELAY = 1500;
+    private static final long SPLASH_SCREEN_DELAY = 3000;
 
 
     @Override
