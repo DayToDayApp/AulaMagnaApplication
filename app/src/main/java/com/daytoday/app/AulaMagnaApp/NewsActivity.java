33af08963c11b07ad70e8a8f9b6204396024072a
Cristina Pérez <pem.cristina@gmail.com>
2017-03-02 13:53:34 +0100

    Removed unused lines

--- app/src/main/java/com/daytoday/app/AulaMagnaApp/NewsActivity.java
+++ app/src/main/java/com/daytoday/app/AulaMagnaApp/NewsActivity.java
@@ -11,7 +11,6 @@ public class NewsActivity extends AppCompatActivity {
     TextView title;
     TextView description;
     TextView date;
-    //TextView category;
     ImageView image;
 
     @Override
@@ -23,14 +22,13 @@ public class NewsActivity extends AppCompatActivity {
         title= (TextView) findViewById(R.id.card_text_title1);
         description= (TextView) findViewById(R.id.card_text_description1);
         date= (TextView) findViewById(R.id.card_text_date1);
-        //category= (TextView) findViewById(R.id.card_text_category1);
         image= (ImageView) findViewById(R.id.card_image_view1);
+
         Bundle bundle = getIntent().getExtras();
 
         image.setImageResource(R.drawable.deportes);
         title.setText(bundle.getString("Title"));
         description.setText(bundle.getString("Text"));
-       // category.setText(bundle.getString("Category"));
         date.setText(bundle.getString("Date"));
 
     }
