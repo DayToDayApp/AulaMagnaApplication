d0c4549e9f7d80dd8bbda523260147eabbaff780
Cristina Pérez <pem.cristina@gmail.com>
2017-03-07 22:13:03 +0100

    Added comments button functionality

--- app/src/main/java/com/daytoday/app/AulaMagnaApp/activities/NewsActivity.java
+++ app/src/main/java/com/daytoday/app/AulaMagnaApp/activities/NewsActivity.java
@@ -1,7 +1,11 @@
 package com.daytoday.app.AulaMagnaApp.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +19,9 @@ public class NewsActivity extends AppCompatActivity {
     TextView description;
     TextView date;
     ImageView image;
+    Button commentsButton;
+
+    String urlCommets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +33,22 @@ public class NewsActivity extends AppCompatActivity {
         description= (TextView) findViewById(R.id.card_text_description1);
         date= (TextView) findViewById(R.id.card_text_date1);
         image= (ImageView) findViewById(R.id.card_image_view1);
+        commentsButton = (Button) findViewById(R.id.activity_news_commets_button);
 
         News news = (News) getIntent().getSerializableExtra("news");
 
         Picasso.with(this).load(news.getImagen()).into(image);
         title.setText(news.getTitle());
         description.setText(news.getText());
+
+        urlCommets = news.getUrlComments();
+
+        commentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse(urlCommets));
+                startActivity(viewIntent);
+            }
+        });
     }
 }
